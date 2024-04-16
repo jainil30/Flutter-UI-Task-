98cbe4bbc9ee8a5afc4ca6cfab3815afdad10c46
Arlieutenant007 <jethvatushar87@gmail.com>
2024-04-16 10:11:31 +0530

    1.0

---
+++ furniture_app/lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff0C8A7B);
+const darkColor = Color(0xff101817);
+const greyColor = Color(0xff828A89);
+const lightGreyColor = Color(0xffF0F0F2);
+const lightTextColor = Colors.white;
+const dartTextColor = Colors.black;
---
+++ furniture_app/lib/constants/data.dart
@@ -0,0 +1,14 @@
+final initData = [
+  {
+    "imageUrl":"assets/images/init_1.png",
+    "text": "View And Exprience Furniture With The Help Of Augmented Reality"
+  },
+  {
+    "imageUrl":"assets/images/init_2.png",
+    "text": "Design Your Space With Augmented Reality By Creating Room"
+  },
+  {
+    "imageUrl":"assets/images/init_3.png",
+    "text": "Explore World Class Top Furnitures As Per Your Requirements & Choice"
+  }
+  ];
