f7d4c2c02743e0a660847a760283a2923eabfb8d
tushar-jethva <jethvatushar87@gmail.com>
2024-04-17 08:33:08 +0530

    home

--- furniture_app/lib/views/home_screen.dart
+++ furniture_app/lib/views/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_app/common/colors.dart';
+import 'package:furniture_app/common/sizes.dart';
 
+/*Created By: Tushar Jethva
+
+*/
 class MyHomeScreen extends StatefulWidget {
   const MyHomeScreen({super.key});
 
@@ -11,6 +16,7 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(preferredSize: Size(double.infinity, getHeight(height: 0.2, context: context)), child: Container(color: white,)),
       body: Center(
         child: Text("Home Screen"),
       ),
